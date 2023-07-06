c1306603283cc8e3201f6a00fa7748eaa29a5e53
oumaymahmina <oumaymahmina2001@gmail.com>
2023-07-06 23:47:55 +0100

    adding variables

--- s3-efs/apps3.py
+++ s3-efs/apps3.py
@@ -7,7 +7,7 @@ s3 = boto3.client('s3')
 def lambda_handler(event, context):
     
     if not os.path.isdir(model_dir):
-        os.system('mkdir /mnt/ml/models/')
+        os.system('mkdir -p /mnt/ml/models/')
 
     bucket_name = event['Records'][0]['s3']['bucket']['name']
     key = event['Records'][0]['s3']['object']['key']
