c6e0d852c0fb6885719a8781e6476ff76caaa318
oumaymahmina <oumaymahmina2001@gmail.com>
2023-07-06 20:45:59 +0100

    bucke$

--- s3-efs/apps3.py
+++ s3-efs/apps3.py
@@ -1,7 +1,6 @@
 import boto3
 import os
 
-
 model_dir = os.getenv('MODEL_DIR', "/mnt/ml/models/")
 s3 = boto3.client('s3')
 
@@ -10,11 +9,11 @@ def lambda_handler(event, context):
     if not os.path.isdir(model_dir):
         os.system('mkdir /mnt/ml/models/')
 
-    bucket_name = bucket-example-mls3
+    bucket_name = event['Records'][0]['s3']['bucket']['name']
     key = event['Records'][0]['s3']['object']['key']
     
     s3.download_file(bucket_name, key, f'{model_dir}/{key}')
     
-    print("ML Model file downloaded;!")
-
+    print("ML Model file downloaded!")
+    
     
