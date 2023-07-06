da6182834386423e0086ac50ac3ca5d0d41311d0
oumaymahmina <oumaymahmina2001@gmail.com>
2023-07-06 20:07:10 +0100

    fixing bucket name

--- s3-efs/apps3.py
+++ s3-efs/apps3.py
@@ -1,18 +1,21 @@
 import boto3
 import os
 
+
 model_dir = os.getenv('MODEL_DIR', "/mnt/ml/models/")
 s3 = boto3.client('s3')
+
+bucket_name = "ml-test-bucket-name"
 
 def lambda_handler(event, context):
     
     if not os.path.isdir(model_dir):
         os.system('mkdir /mnt/ml/models/')
 
-    bucket_name = event['Records'][0]['s3']['bucket']['name']
     key = event['Records'][0]['s3']['object']['key']
     
     s3.download_file(bucket_name, key, f'{model_dir}/{key}')
     
     print("ML Model file downloaded;!")
+
     
